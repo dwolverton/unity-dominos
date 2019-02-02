0bf538334e77e91361b6422940367122731499c0
David Wolverton <dwolvert@gmail.com>
2019-02-02 17:53:15 -0500

    Camera follows bouncy yellow capsule

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public GameObject target;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 v = target.transform.position + offset;
+		v.y = 5.46f;
+		transform.position = v;
+	}
+}
