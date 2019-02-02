3914021873d0a2b8a8e39749a36e654bda7a4350
David Wolverton <dwolvert@gmail.com>
2019-02-02 15:18:16 -0500

    initial commit

---
+++ Assets/AddDominosScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddDominosScript : MonoBehaviour {
+	
+    public GameObject template;
+
+	// Use this for initialization
+	void Start()
+	{
+		for (int i = 0; i < 30; i++)
+		{
+			//Debug.Log("Hello");
+			Instantiate(template, new Vector3(0, 1.5F, i * 3F + 3), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
